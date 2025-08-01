c12fe44abe68a099d76fdc56a20d6c58fc55df79
sameerazam <sameerazamned@gmail.com>
2025-08-01 16:54:56 +0500

    Add prac 3 and 4

---
+++ prac3/app.js
@@ -0,0 +1,11 @@
+var age = 25;
+alert("I am " + age + " years old");
+var visitCount = 5; // You can update this number dynamically using cookies or localStorage
+document.write("You have visited this site " + visitCount + " times");
+var birthYear = 2000;
+document.write("My birth year is " + birthYear + "<br>");
+document.write("Data type of my declared variable is " + typeof birthYear);
+var visitorName = "John Doe";
+var productTitle = "T-shirt(s)";
+var quantity = 5;
+document.write(visitorName + " ordered " + quantity + " " + productTitle + " on XYZ Clothing store");
---
+++ prac4/app.js
@@ -0,0 +1,11 @@
+var name, age, country;
+var userName;
+var $price;
+var _value;
+var myVar123;
+var camelCase;
+// var 123name;    // starts with a number
+// var my-name;    // contains hyphen
+// var var;        // reserved keyword
+// var user name;  // contains space
+// var @value;     // starts with special character not allowed
