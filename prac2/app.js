f9141ac03342c55310b29bdad5c66c731a069079
sameerazam <sameerazamned@gmail.com>
2025-08-01 16:29:23 +0500

    Initial commit

---
+++ prac2/app.js
@@ -0,0 +1,15 @@
+// var username;
+// var myname="sameer;"
+// var message=Tmkoc;
+// alert(message);
+// var stdname="sameer";
+// var age=19;
+// var course="Mobile app dev"
+// alert(stdname);
+// alert(age);
+// alert(course);
+// var pizza ="pizza\npizz\npiz\npi\np";
+// alert(pizza);
+// var email="sameerazamned@gmail.com";
+// alert("MY email is"+ email);
+document.write("how are you")
---
+++ pract1/app.js
@@ -0,0 +1,4 @@
+alert("Error! Please enter a valid password\nWelcome to jsland");
+  alert("Welcome to JS Land...");
+  alert("Happy Coding!");
+console.log("hello i can acees js using cosnloe");
